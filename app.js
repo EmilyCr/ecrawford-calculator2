003e295a3582d2e344b14c3322d69896a600e066
EmilyCr <crawford.emily@hotmail.com>
2015-03-26 17:10:56 -0400

    put

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+// Use calculator in the URL
 app.use('/calculator', calculator);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Assignment 1', image: '/images/Calculatordude.jpg' });
+  res.render('index', { title: 'Assignment 1' });
 });
 
 module.exports = router;
